b3dc35feef269201d28e6301908e412b3582e998
Batu <batusalkim11@gmail.com>
2025-01-01 09:36:02 +0300

    Update App.jsx

--- kykyemek/src/App.jsx
+++ kykyemek/src/App.jsx
@@ -4,9 +4,11 @@ import "./MobileMealPlanner.css";
 export default function MaintenancePage() {
   return (
     <div class="status-container">
-  <span class="status-dot"></span>
-  <span class="status-message">Site Bakımda - 2 Gün Sonra Aktif Olacak</span>
+  <div class="status-dot"></div>
+  <div class="status-message">
+    <span class="moving-text">Site Bakımda</span>
+    <span class="fixed-text"> - 2 gün sonra aktif olacak</span>
+  </div>
 </div>
-
   );
 }
