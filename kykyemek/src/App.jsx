ab6fb600ebc12a25cc9f915c23363e950b763abb
Batu <batusalkim11@gmail.com>
2025-01-01 09:24:03 +0300

    Update App.jsx

--- kykyemek/src/App.jsx
+++ kykyemek/src/App.jsx
@@ -5,8 +5,7 @@ export default function MaintenancePage() {
   return (
     <div className="maintenance-container">
       <h1 className="maintenance-message">Site Bakımda</h1>
+      <p className="maintenance-submessage">2 gün sonra aktif olacak</p>
     </div>
   );
 }
-
-
