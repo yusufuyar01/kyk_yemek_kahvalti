427c467ef2f327be871b9657c68647d160332553
batuhan <batusalkim11@gmail.com>
2025-01-11 12:42:26 +0300

    hatalar düzeltiliyor

---
+++ kykyemek/public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'kyk-yemek-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/aralik.json',
+    '/favicon.ico',
+    '/logo192.png',
+    '/logo512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+}); 
